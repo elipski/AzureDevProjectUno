db320851553a9857dc015faa072ba7a9f61a7fe6
Ed Lipski <elipski@gmail.com>
2022-04-13 20:24:12 -0400

    Updated login successful/unsuccessful log messages

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -77,11 +77,11 @@ def login():
         if user is None or not user.check_password(form.password.data):
             logging.info('Invalid login attempt')
             return redirect(url_for('login'))
-        logging.info('logged in successfully')
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('home')
+        logging.info('logged in successfully')
         return redirect(next_page)
     session["state"] = str(uuid.uuid4())
     auth_url = _build_auth_url(scopes=Config.SCOPE, state=session["state"])
